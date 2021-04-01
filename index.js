e735881bcc9c515a8a0e1b7bd1df560408b192fd
Misha <67483963+mishawali@users.noreply.github.com>
2021-04-01 13:56:27 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,54 @@
+
+let jsonURL = "https://raw.githubusercontent.com/mishawali/assignment3-js/main/products.json";
+
+
+//use fetch to access the json information
+fetch(jsonURL).then(function(response) {
+    return response.json();
+  }).then(function(data) {
+    console.log(data.length);
+    appendData(data);
+  });
+  
+
+
+  function appendData(data) {
+  let tbody = document.getElementById('productListBody');
+
+  for(let i = 0; i < data.length; i++) {
+
+  //create a table row element
+  let tr = document.createElement("tr");
+  
+      //Create columns
+      let imgTd = document.createElement("td");
+      let productnameTd = document.createElement("td");
+      let descriptionTd = document.createElement("td");
+      let priceTd = document.createElement("td");
+
+
+      
+    //productname
+    productnameTd.innerHTML = data[i].productName;
+    //price
+    priceTd.innerHTML = "$" + data[i].price;
+    //description
+    descriptionTd.innerHTML = data[i].description;
+    //img
+    let img = document.createElement("img");
+    img.setAttribute("src", data[i].image);
+    img.style.width = "50px";
+    imgTd.appendChild(img);
+
+
+
+     //append columns to a table row
+     tr.appendChild(imgTd);
+     tr.appendChild( productnameTd);
+     tr.appendChild(descriptionTd);
+     tr.appendChild(priceTd);
+  
+      //append a table row to the tbody
+      tbody.appendChild(tr);
+    }
+  }
